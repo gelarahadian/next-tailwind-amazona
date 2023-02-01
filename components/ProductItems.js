a7885dcff6669cab052253e77b1a9b24306f97ab
gelarahadian <gelarahadian38@gmail.com>
2023-02-01 16:50:35 +0700

    load product mongodb

--- components/ProductItems.js
+++ components/ProductItems.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-export default function ProductItems({ product }) {
+export default function ProductItems({ product, addToCartHandler }) {
   return (
     <div className="card">
       <div className="w-full h-52 relative">
@@ -15,8 +15,13 @@ export default function ProductItems({ product }) {
           <h2 className="text-lg">{product.name}</h2>
         </Link>
         <p className="mb-2">{product.brand}</p>
-        <p>{product.price}</p>
-        <button className="primary-button">add to cart</button>
+        <p>${product.price}</p>
+        <button
+          className="primary-button mt-2"
+          onClick={() => addToCartHandler(product)}
+        >
+          add to cart
+        </button>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
 import ProductItems from '@/components/ProductItems';
-import data from '@/utils/data';
+import Product from '@/models/Product';
+import db from '@/utils/db';
+import { Store } from '@/utils/Store';
+import axios from 'axios';
+import { useContext } from 'react';
+import { toast } from 'react-toastify';
 import Layout from '../components/Layout';
 
-export default function Home() {
-  let products = data.products.map((product) => (
-    <ProductItems product={product} key={product.slug} />
-  ));
+export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry. Product is out of stock');
+    }
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    toast.success('Product added to the cart');
+  };
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {products}
+        {products.map((product) => (
+          <ProductItems
+            product={product}
+            key={product.slug}
+            addToCartHandler={addToCartHandler}
+          />
+        ))}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
